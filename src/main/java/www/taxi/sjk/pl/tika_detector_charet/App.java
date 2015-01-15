6d3e3fe28809ec5538c210d577dfce0e43054bd0
Sebastian Jakowski <sebastian.jakowski@beneering.com>
2015-01-15 10:35:13 +0100

    Change the methods

--- src/main/java/www/taxi/sjk/pl/tika_detector_charet/App.java
+++ src/main/java/www/taxi/sjk/pl/tika_detector_charet/App.java
@@ -1,25 +1,15 @@
 package www.taxi.sjk.pl.tika_detector_charet;
 
-import java.io.IOException;
 
 /**
- * Hello world!
+ * 
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        TikaDetectorCharSet detector = new TikaDetectorCharSet();
-        try {
-			detector.checkEncoding("ANSI.csv");
-			
-			detector.checkEncoding("UTF.csv");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-     
-    }
+public class App {
+	public static void main(String[] args) {
+		TikaDetectorCharSet detector = new TikaDetectorCharSet();
+		detector.getEncoding("ANSI.csv");
+		detector.getEncoding("UTF.csv");
+
+	}
 }
--- src/main/java/www/taxi/sjk/pl/tika_detector_charet/TikaDetectorCharSet.java
+++ src/main/java/www/taxi/sjk/pl/tika_detector_charet/TikaDetectorCharSet.java
@@ -3,6 +3,7 @@ package www.taxi.sjk.pl.tika_detector_charet;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,19 +12,32 @@ import org.apache.tika.parser.txt.CharsetMatch;
 
 public class TikaDetectorCharSet {
 
-	public String checkEncoding(String filename) throws IOException{
-		
+	private String checkEncoding(InputStream input){
 				
 		CharsetDetector detector = new CharsetDetector();
-		InputStream str = new FileInputStream(new File(filename));
-		InputStream toDetect = new BufferedInputStream(str);
-		detector.setText(toDetect);
+		try {
+			detector.setText(input);
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		}
 		CharsetMatch charset = detector.detect();
-		
-		System.out.println(charset.getName());
-		
-		return charset.getName();
-		
+		return charset.getName();	
+	}
+	
+	public String getEncoding(String filename){
+		InputStream str;
+		String encoding;
+		try {
+			str = new FileInputStream(new File(filename));
+			InputStream toDetect = new BufferedInputStream(str);
+			encoding = checkEncoding(toDetect);
+			return encoding;
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	
